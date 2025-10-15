5f705d6959aa6c02c0a6584612726f5943485fe7
Shawon <shawoncodes@gmail.com>
2025-10-14 20:15:06 -0400

    book

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text():
+    with open(path_to_file) as f:
+        # f is a file object
+        file_contents = f.read()
+    return file_contents
+
+path_to_file = "/Users/shawn/Documents/bootdev/bookbot/books/frankenstein.txt"
+
+
+def main():
+    book_text = get_book_text()
+    print(book_text)
+
+main()
+
